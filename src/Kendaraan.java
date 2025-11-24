01e1f3192bbced024d4cbb4f1220486dba5d3ab0
firdausinzzl <firdausin007@gmail.com>
2025-11-24 13:55:55 +0700

    Kendaraan

---
+++ src/Kendaraan.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lab Studio
+ */
+public class Kendaraan {
+      
+    private String noPolisi;
+    private String merk;
+    private int tahun;
+
+    public void setNoPolisi(String noPolisi) {
+        this.noPolisi = noPolisi;
+    }
+
+    public String getNoPolisi() {
+        return this.noPolisi;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getMerk() {
+        return this.merk;
+    }
+
+    public void setTahun(int tahun) {
+        this.tahun = tahun;
+    }
+
+    public int getTahun() {
+        return this.tahun;
+    }
+}
