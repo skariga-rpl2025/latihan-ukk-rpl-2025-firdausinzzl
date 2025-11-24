07668cc680e0ea6fa13933e3b156d4d823f52132
firdausinzzl <firdausin007@gmail.com>
2025-11-24 13:50:49 +0700

    Karyawan

---
+++ src/Karyawan.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lab Studio
+ */
+public class Karyawan {
+    
+    private String NIP;
+    private String nama;
+    private String divisi;
+
+    public void setNIP(String NIP) {
+        this.NIP = NIP;
+    }
+
+    public String getNIP() {
+        return this.NIP;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public void setDivisi(String divisi) {
+        this.divisi = divisi;
+    }
+
+    public String getDivisi() {
+        return this.divisi;
+    }
+}
+
